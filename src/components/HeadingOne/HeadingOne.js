029058b0a864a1e3ca7a5b51772931c5a0be7afb
jarekTerejko <j.terejko@gmail.com>
2021-11-09 10:59:25 +0100

    add HeadingOne

---
+++ src/components/HeadingOne/HeadingOne.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const HeadingOne = styled.h1`
+  font-size: 15rem;
+  text-transform: uppercase;
+  font-weight: 400;
+
+  span.intro {
+    font-size: 2.8rem;
+    letter-spacing: 4.75px;
+    color: var(--color-light-violet);
+    display: block;
+    margin-bottom: 2.4rem;
+  }
+  span.heading {
+    display: block;
+    font-family: var(--font-family-serif);
+    margin-bottom: 2.4rem;
+  }
+`;
