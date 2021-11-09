d5bc58ea1a8deacc6f889fb47190fc551328cff1
jarekTerejko <j.terejko@gmail.com>
2021-11-09 10:58:51 +0100

    add TextRegular

---
+++ src/components/TextRegular/TextRegularElement.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const TextRegular = styled.p`
+  font-size: 1.8rem;
+  line-height: 28.5px;
+  letter-spacing: 1.3px;
+  max-width: 43rem;
+  color: var(--color-light-violet);
+`;
