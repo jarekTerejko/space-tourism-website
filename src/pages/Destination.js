c1aee2b7f257d98e3c71bd0bc8efefc62b4f3ccd
jarekTerejko <j.terejko@gmail.com>
2021-11-15 04:14:52 +0100

    add DestinationContent to Destination page

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,13 +1,24 @@
 import useFetch from "../useFetch";
 import Loader from "../components/Loader";
+import { PageBg } from "../components/PageBg/PageBgElement";
+import DestinationContent from "../components/DestinationContent";
 const Destination = () => {
   const { data, isLoading } = useFetch("../data.json");
-  //   console.log(data);
 
   return (
     <>
       {isLoading ? <Loader /> : null}
-      {data.length === 0 ? null : <h1>Destination</h1>}
+      {data.length === 0 ? null : (
+        <>
+          <PageBg
+            homeDesktopBg={data[0].bgs[1].desktop}
+            homeTabletBg={data[0].bgs[1].tablet}
+            homeMobileBg={data[0].bgs[1].mobile}
+          >
+            <DestinationContent data={data}></DestinationContent>
+          </PageBg>
+        </>
+      )}
     </>
   );
 };
