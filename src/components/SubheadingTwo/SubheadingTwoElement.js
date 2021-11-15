9386c24cf3cec0f6a7da54778394df0fff785399
jarekTerejko <j.terejko@gmail.com>
2021-11-15 03:24:43 +0100

    add SubheadingTwo component

---
+++ src/components/SubheadingTwo/SubheadingTwoElement.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SubheadingTwo = styled.h4`
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 1.7rem;
+  letter-spacing: 2.3625px;
+  text-transform: uppercase;
+  color: var(--color-light-violet);
+  margin-bottom: 1.2rem;
+`;
