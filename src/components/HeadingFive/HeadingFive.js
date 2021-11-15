913ca29b236a15f6982ea9a070417c3eed116962
jarekTerejko <j.terejko@gmail.com>
2021-11-15 03:22:55 +0100

    add HeadingFive component

---
+++ src/components/HeadingFive/HeadingFive.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const HeadingFive = styled.h5`
+  font-family: var(--font-family-sans);
+  font-weight: 400;
+  font-size: 2.8rem;
+  line-height: 3.4rem;
+  letter-spacing: 0.4725rem;
+  text-transform: uppercase;
+
+  span.counter {
+    color: rgba(255, 255, 255, 0.25);
+    display: inline-block;
+    margin-right: 2.8rem;
+  }
+`;
