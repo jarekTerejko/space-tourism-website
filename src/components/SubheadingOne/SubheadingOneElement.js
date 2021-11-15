efd0c704f2f9175a5386388b95cf85d5c66ff1c5
jarekTerejko <j.terejko@gmail.com>
2021-11-15 03:24:23 +0100

    add SubheadingOne component

---
+++ src/components/SubheadingOne/SubheadingOneElement.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SubheadingOne = styled.h3`
+  font-family: var(--font-family-serif);
+  font-weight: 400;
+  font-size: 2.8rem;
+  line-height: 3.2rem;
+  text-transform: uppercase;
+`;
