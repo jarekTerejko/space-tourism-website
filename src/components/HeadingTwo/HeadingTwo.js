6e48518ad814b6574000b397526aafb462af6309
jarekTerejko <j.terejko@gmail.com>
2021-11-15 03:23:27 +0100

    add HeadingTwo component

---
+++ src/components/HeadingTwo/HeadingTwo.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeadingTwo = styled.h2`
+  font-family: var(--font-family-serif);
+  font-weight: 400;
+  font-size: 10rem;
+  line-height: 11.5rem;
+  text-transform: uppercase;
+  margin-bottom: 1.4rem;
+`;
