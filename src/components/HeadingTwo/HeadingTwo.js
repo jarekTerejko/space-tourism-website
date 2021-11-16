25e5c40726b5936fa3265d2ce90e37c8a33b6b77
jarekTerejko <j.terejko@gmail.com>
2021-11-16 08:35:55 +0100

    add media queries for tablet and mobile

--- src/components/HeadingTwo/HeadingTwo.js
+++ src/components/HeadingTwo/HeadingTwo.js
@@ -7,4 +7,15 @@ export const HeadingTwo = styled.h2`
   line-height: 11.5rem;
   text-transform: uppercase;
   margin-bottom: 1.4rem;
+
+  @media screen and (max-width: 1150px) {
+    font-size: 8rem;
+    line-height: 92px;
+    margin-bottom: 0.8rem;
+  }
+
+  @media screen and (max-width: 400px) {
+    font-size: 5.6rem;
+    line-height: 6.4rem;
+  }
 `;
