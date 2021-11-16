febce87b2d5546ff3696e6cad3da9acaf63ec6a3
jarekTerejko <j.terejko@gmail.com>
2021-11-16 08:41:40 +0100

    add style to logo in Navbar

--- src/components/Image/ImageElement.js
+++ src/components/Image/ImageElement.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const ImageEl = styled.img``;
+export const ImageEl = styled.img`
+  &.logo {
+    @media screen and (max-width: 400px) {
+      max-width: 4rem;
+    }
+  }
+`;
