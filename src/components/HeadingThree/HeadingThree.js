512554d3ee649030d0dfbade2b418b3ba5a4f186
jarekTerejko <j.terejko@gmail.com>
2021-11-29 04:44:26 +0100

    add HeadingThree media queries for Technology page

--- src/components/HeadingThree/HeadingThree.js
+++ src/components/HeadingThree/HeadingThree.js
@@ -7,13 +7,28 @@ export const HeadingThree = styled.h3`
   line-height: 64px;
   text-transform: uppercase;
   margin-bottom: ${({ roleHeading }) => (roleHeading ? "2.7rem" : "")};
-  margin-bottom: ${({ technologyHeading }) => (technologyHeading ? "1.7rem" : "")};
+  margin-bottom: ${({ technologyHeading }) =>
+    technologyHeading ? "1.7rem" : ""};
 
   @media screen and (max-width: 945px) {
     font-size: ${({ roleHeading }) => (roleHeading ? "4rem" : "")};
     line-height: ${({ roleHeading }) => (roleHeading ? "46px" : "")};
     margin-bottom: ${({ roleHeading }) => (roleHeading ? "1.6rem" : "")};
   }
+
+  @media screen and (max-width: 768px) {
+    font-size: ${({ technologyHeading }) => (technologyHeading ? "4rem" : "")};
+    line-height: ${({ technologyHeading }) =>
+      technologyHeading ? "4.6rem" : ""};
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: ${({ technologyHeading }) =>
+      technologyHeading ? "2.4rem" : ""};
+    line-height: ${({ technologyHeading }) =>
+      technologyHeading ? "2.8rem" : ""};
+  }
+
   @media screen and (max-width: 430px) {
     font-size: ${({ roleHeading }) => (roleHeading ? "2.4rem" : "")};
     line-height: ${({ roleHeading }) => (roleHeading ? "2.8rem" : "")};
