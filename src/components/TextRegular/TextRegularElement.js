0023b4b64021762d740bfe13ae5bb11e0c6ed934
jarekTerejko <j.terejko@gmail.com>
2021-12-03 10:14:52 +0100

    add media queries for Destination page

--- src/components/TextRegular/TextRegularElement.js
+++ src/components/TextRegular/TextRegularElement.js
@@ -13,11 +13,6 @@ export const TextRegular = styled.p`
     font-size: 1.6rem;
   }
 
-  @media screen and (max-width: 375px) {
-    font-size: 1.5rem;
-    line-height: 25px;
-  }
-
   @media screen and (max-width: 850px) {
     margin: ${({ homepageText }) => (homepageText ? "0 auto" : "")};
     max-width: ${({ homepageText }) => (homepageText ? "44rem" : "")};
@@ -35,6 +30,13 @@ export const TextRegular = styled.p`
     margin-bottom: 4.9rem;
   }
 
+  @media screen and (max-width: 375px) {
+    min-height: ${({ destinationPageText }) =>
+      destinationPageText ? "12.5rem" : ""};
+   margin-bottom: ${({ destinationPageText }) =>
+      destinationPageText ? "3.2rem" : ""};
+  }
+  
   @media screen and (max-width: 945px) {
     max-width: ${({ crewPageText }) => (crewPageText ? "60rem" : "")};
     font-size: ${({ crewPageText }) => (crewPageText ? "1.6rem" : "")};
@@ -43,5 +45,10 @@ export const TextRegular = styled.p`
   @media screen and (max-width: 375px) {
     font-size: ${({ crewPageText }) => (crewPageText ? "1.5rem" : "")};
     line-height: ${({ crewPageText }) => (crewPageText ? "2.5rem" : "")};
+  }
+
+  @media screen and (max-width: 375px) {
+    font-size: 1.5rem;
+    line-height: 2.5rem;
   }
 `;
